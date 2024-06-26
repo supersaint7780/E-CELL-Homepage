06ffcca14804c6aade6852ea89d40fbd85857914
Mohammad Maasir <maasir554@gmail.com>
2024-06-26 23:47:07 +0530

    updated sponsors' section on main page

---
+++ populate-sponsors.js
@@ -0,0 +1,425 @@
+const spnData = [
+    {
+      name: 'adani',
+      website: 'https://www.adani.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/adani.webp'
+    },
+    {
+      name: 'rupeezy',
+      website: 'https://rupeezy.in/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/rupeexy.webp'
+    },
+    {
+      name: 'bhopalangels',
+      website: 'https://bhopalangels.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/bhopal_angels.webp'
+    },
+    {
+      name: 'aicrntu',
+      website: 'https://www.aicrntu.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/aicrntu.webp'
+    },
+    {
+      name: 'boudhikip',
+      website: 'https://www.boudhikip.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/boudhik.webp'
+    },
+    {
+      name: 'aws',
+      website: 'https://aws.amazon.com/free/?gclid=Cj0KCQiA1rSsBhDHARIsANB4EJY0bGmloslMy81LQFkrUL0lfh-8wxcPyyt4wAj1DBF4uvhn7-19soQaAjx2EALw_wcB&trk=14a4002d-4936-4343-8211-b5a150ca592b&sc_channel=ps&ef_id=Cj0KCQiA1rSsBhDHARIsANB4EJY0bGmloslMy81LQFkrUL0lfh-8wxcPyyt4wAj1DBF4uvhn7-19soQaAjx2EALw_wcB:G:s&s_kwcid=AL!4422!3!453325184782!e!!g!!aws!10712784856!111477279771&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*all',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/aws.webp'
+    },
+    {
+      name: 'coingape',
+      website: 'https://coingape.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/coingape.png'
+    },
+    {
+      name: 'givemycertificate',
+      website: 'https://givemycertificate.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/arrow.webp'
+    },
+    {
+      name: 'canva',
+      website: 'https://www.canva.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/canva.webp'
+    },
+    {
+      name: 'icreate',
+      website: 'https://www.icreate.org.in/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/icreate.webp'
+    },
+    {
+      name: 'jiosaavn',
+      website: 'https://www.jiosaavn.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/jiosaavn.webp'
+    },
+    {
+      name: 'kitablovers',
+      website: 'https://kitablovers.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/kitab_lovers.webp'
+    },
+    {
+      name: 'msg91',
+      website: 'https://msg91.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/msg91.webp'
+    },
+    {
+      name: 'papertrade',
+      website: 'https://papertrade.live/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/papertrade.webp'
+    },
+    {
+      name: 'recklabs',
+      website: 'https://recklabs.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/RECKLABS.webp'
+    },
+    {
+      name: 'startupvisors',
+      website: 'https://startupvisors.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/startupvisors.webp'
+    },
+    {
+      name: 'taskade',
+      website: 'https://www.taskade.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/taskade.webp'
+    },
+    {
+      name: 'teachnook',
+      website: 'https://teachnook.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/teachnook.webp'
+    },
+    {
+      name: 'techstory',
+      website: 'https://techstory.in/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/techstory.webp'
+    },
+    {
+      name: 'unstop',
+      website: 'https://unstop.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/unstop.webp'
+    },
+    {
+      name: 'wolframalpha',
+      website: 'https://www.wolframalpha.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/wolfram_research.webp'
+    },
+    {
+      name: 'sixthsenseit',
+      website: 'https://sixthsenseit.com/',
+      imageSource: 'https://esummit.ecellnitb.in/assets/sponsor/sixthsenselogo.webp'
+    },
+    {
+      name: 'skill-lync',
+      website: 'https://skill-lync.com/',
+      imageSource: './assets/images/sponsors/Asset%205.webp'
+    },
+    {
+      name: 'ibentos',
+      website: 'https://ibentos.com/',
+      imageSource: './assets/images/sponsors/Asset%206.webp'
+    },
+    {
+      name: 'edvoy',
+      website: 'https://edvoy.com/',
+      imageSource: './assets/images/sponsors/Asset%207.webp'
+    },
+    {
+      name: 'freshworks',
+      website: 'https://www.freshworks.com/',
+      imageSource: './assets/images/sponsors/Asset%209.webp'
+    },
+    {
+      name: 'startuplab',
+      website: 'https://startuplab.no/',
+      imageSource: './assets/images/sponsors/Asset%2011.webp'
+    },
+    {
+      name: 'prepbytes',
+      website: 'https://www.prepbytes.com/',
+      imageSource: './assets/images/sponsors/Asset%2012.webp'
+    },
+    {
+      name: 'geeksforgeeks',
+      website: 'https://www.geeksforgeeks.org/',
+      imageSource: './assets/images/sponsors/Asset%2013.webp'
+    },
+    {
+      name: 'codingninjas',
+      website: 'https://www.codingninjas.com/',
+      imageSource: './assets/images/sponsors/Asset%2014.webp'
+    },
+    {
+      name: 'primexbt',
+      website: 'https://primexbt.com/',
+      imageSource: './assets/images/sponsors/Asset%2015.webp'
+    },
+    {
+      name: 'codingblocks',
+      website: 'https://codingblocks.com/',
+      imageSource: './assets/images/sponsors/Asset%2016.webp'
+    },
+    {
+      name: 'polygon',
+      website: 'https://www.polygon.com/',
+      imageSource: './assets/images/sponsors/Asset%2018.webp'
+    },
+    {
+      name: 'athenasquare',
+      website: 'https://athenasquare.org/',
+      imageSource: './assets/images/sponsors/Asset%2019.webp'
+    },
+    {
+      name: 'finlatics',
+      website: 'https://www.finlatics.com/',
+      imageSource: './assets/images/sponsors/Asset%2021.webp'
+    },
+    {
+      name: 'celo',
+      website: 'https://celo.org/',
+      imageSource: './assets/images/sponsors/Asset%2022.webp'
+    },
+    {
+      name: 'elearnmarkets',
+      website: 'https://www.elearnmarkets.com/',
+      imageSource: './assets/images/sponsors/Asset%2023.webp'
+    },
+    {
+      name: 'indiaaccelerator',
+      website: 'https://www.indiaaccelerator.co/',
+      imageSource: './assets/images/sponsors/Asset%2024.webp'
+    },
+    {
+      name: 'ecellnitb',
+      website: 'https://www.ecellnitb.in/#',
+      imageSource: './assets/images/sponsors/Asset%2025.webp'
+    },
+    {
+      name: 'workspace',
+      website: 'http://www.workspace.com/',
+      imageSource: './assets/images/sponsors/Asset%2026.webp'
+    },
+    {
+      name: 'skwerup',
+      website: 'https://www.skwerup.com/',
+      imageSource: './assets/images/sponsors/Asset%2027.webp'
+    },
+    {
+      name: 'devfolio',
+      website: 'https://devfolio.co/hackathons/applied',
+      imageSource: './assets/images/sponsors/Asset%2028.webp'
+    },
+    {
+      name: 'filecoin',
+      website: 'https://filecoin.io/',
+      imageSource: './assets/images/sponsors/Asset%2029.webp'
+    },
+    {
+      name: 'notesnaka',
+      website: 'https://www.notesnaka.com/homepage/',
+      imageSource: './assets/images/sponsors/Asset%2030.webp'
+    },
+    {
+      name: 't3',
+      website: 'https://www.t3.com/',
+      imageSource: './assets/images/sponsors/Asset%2031.webp'
+    },
+    {
+      name: 'ecellnitb',
+      website: 'https://www.ecellnitb.in/#',
+      imageSource: './assets/images/sponsors/Asset%2032.webp'
+    },
+    {
+      name: 'linuxlinks',
+      website: 'https://www.linuxlinks.com/linux-world/',
+      imageSource: './assets/images/sponsors/Asset%2033.webp'
+    },
+    {
+      name: 'icreateedu',
+      website: 'https://www.icreateedu.com/',
+      imageSource: './assets/images/sponsors/Asset%2034.webp'
+    },
+    {
+      name: 'digitalocean',
+      website: 'https://www.digitalocean.com/',
+      imageSource: './assets/images/sponsors/Asset%2035.webp'
+    },
+    {
+      name: 'codingminutes',
+      website: 'https://codingminutes.com/',
+      imageSource: './assets/images/sponsors/Asset%2036.webp'
+    },
+    {
+      name: 'codingminutes',
+      website: 'https://codingminutes.com/',
+      imageSource: './assets/logo/3ways.webp'
+    },
+    {
+      name: 'codingminutes',
+      website: 'https://codingminutes.com/',
+      imageSource: './assets/logo/ApnaCast.webp'
+    },
+    {
+      name: 'ecellnitb',
+      website: 'https://www.ecellnitb.in/#',
+      imageSource: './assets/logo/bob.webp'
+    },
+    {
+      name: 'bansalgroup',
+      website: 'https://bansalgroup.in/',
+      imageSource: './assets/logo/bansal.webp'
+    },
+    {
+      name: 'codingminutes',
+      website: 'https://codingminutes.com/',
+      imageSource: './assets/logo/bv.webp'
+    },
+    {
+      name: 'coolberg',
+      website: 'https://coolberg.in/',
+      imageSource: './assets/logo/coolberg.webp'
+    },
+    {
+      name: 'crowdera',
+      website: 'https://www.crowdera.com/',
+      imageSource: './assets/logo/crowdera.webp'
+    },
+    {
+      name: 'easemytrip',
+      website: 'https://www.easemytrip.com/',
+      imageSource: './assets/logo/easymytrip.webp'
+    },
+    {
+      name: 'emb',
+      website: 'https://emb.global/',
+      imageSource: './assets/logo/Expand%20My%20Business.webp'
+    },
+    {
+      name: 'forethought',
+      website: 'https://www.forethought.co.in/',
+      imageSource: './assets/logo/forethought.webp'
+    },
+    {
+      name: 'freshworks',
+      website: 'https://www.freshworks.com/',
+      imageSource: './assets/logo/freashworks.webp'
+    },
+    {
+      name: 'fantasytradingleague',
+      website: 'https://fantasytradingleague.com/',
+      imageSource: './assets/logo/ftlt.webp'
+    },
+    {
+      name: 'goodworks',
+      website: 'https://www.goodworks.in/',
+      imageSource: './assets/logo/goodworks_cowork_logo.webp'
+    },
+    {
+      name: 'hindustanpetroleum',
+      website: 'https://www.hindustanpetroleum.com/',
+      imageSource: './assets/logo/hpcl.webp'
+    },
+    {
+      name: 'innovativescripts',
+      website: 'https://innovativescripts.com/',
+      imageSource: './assets/logo/inovative%20script.webp'
+    },
+    {
+      name: 'iocl',
+      website: 'https://iocl.com/',
+      imageSource: './assets/logo/iocl.webp'
+    },
+    {
+      name: 'learningwhiletravelling',
+      website: 'https://learningwhiletravelling.com/',
+      imageSource: './assets/logo/Learning%20While%20Traveling.webp'
+    },
+    {
+      name: 'mguindia',
+      website: 'https://www.mguindia.com/',
+      imageSource: './assets/logo/mansarovar.webp'
+    },
+    {
+      name: 'meity',
+      website: 'https://www.meity.gov.in/',
+      imageSource: './assets/logo/MEITY.webp'
+    },
+    {
+      name: 'startup',
+      website: 'https://startup.mp.gov.in/',
+      imageSource: './assets/logo/mpsu.webp'
+    },
+    {
+      name: 'panmacmillan',
+      website: 'https://www.panmacmillan.co.in/',
+      imageSource: './assets/logo/pan%20macmillan.webp'
+    },
+    {
+      name: 'pedalstart',
+      website: 'https://pedalstart.com/index.html',
+      imageSource: './assets/logo/PedalStart.webp'
+    },
+    {
+      name: 'quillbot',
+      website: 'https://quillbot.com/',
+      imageSource: './assets/logo/quillbot.webp'
+    },
+    {
+      name: 'realvision',
+      website: 'https://www.realvision.com/india',
+      imageSource: './assets/logo/Real%20Vision.webp'
+    },
+    {
+      name: 'redfmindia',
+      website: 'https://www.redfmindia.in/',
+      imageSource: './assets/logo/redfm.webp'
+    },
+    {
+      name: 'roostoo',
+      website: 'https://www.roostoo.com/',
+      imageSource: './assets/logo/roostoo.webp'
+    },
+    {
+      name: 'codingminutes',
+      website: 'https://codingminutes.com/',
+      imageSource: './assets/logo/sf.webp'
+    },
+    {
+      name: 'starworks',
+      website: 'https://starworks.world/',
+      imageSource: './assets/logo/starworks_prime_logo.webp'
+    },
+    {
+      name: 'stockwealthacademy',
+      website: 'https://stockwealthacademy.com/',
+      imageSource: './assets/logo/stock%20wealth%20academy.webp'
+    },
+    {
+      name: 'stpi',
+      website: 'https://stpi.in/',
+      imageSource: './assets/logo/stpi.webp'
+    },
+    {
+      name: 'webodoctor',
+      website: 'https://www.webodoctor.com/',
+      imageSource: './assets/logo/Webodoctor.webp'
+    }
+  ]
+
+const mainContainer = document.getElementById("sponsorIcons");
+mainContainer.innerHTML = ""
+
+spnData.forEach(sponsor => {
+    const spnImg = new Image()
+    spnImg.src = sponsor.imageSource;
+    mainContainer.innerHTML += 
+    `
+        <div class="sponsorItem mob-sponsorItem" data-sponsorname=${sponsor.name}>
+            <a href=${sponsor.website}>    
+            <img class="${spnImg.naturalWidth/spnImg.naturalHeight>175/135?'horizontal':'vertical'} mob-img1" src=${sponsor.imageSource} alt=${sponsor.name}>
+            </a>
+        </div>
+    `;
+    // Image original aspect ratio will be compared with the aspect ratio of container(175/135 in this case)
+    // to determine the limiting dimension.
+})
